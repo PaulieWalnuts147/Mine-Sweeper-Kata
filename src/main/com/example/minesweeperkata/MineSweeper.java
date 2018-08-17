552f5d9ec86bfd29d3dd8acbe686e072df14f3f0
davidn <david.noorshargh@mkodo.com>
2018-08-17 11:36:56 +0100

    refactoring  if statements

--- src/main/com/example/minesweeperkata/MineSweeper.java
+++ src/main/com/example/minesweeperkata/MineSweeper.java
@@ -10,60 +10,82 @@ public class MineSweeper {
     public void sweep() {
         for(int row=0; row<board.length; row++) {
             for(int column=0; column<board[row].length; column++) {
-                int bombsInTheVicinity = 0;
-                if(board[row][column] != '*') {
-                    if (thereIsAColumnToTheLeft(column) && neighbouringSquareContainsABomb(row, column - 1)) { //left
-                        bombsInTheVicinity++;
-                        updateBombDistanceValue(row, column, bombsInTheVicinity);
-                    }
-                    if (thereIsAColumnToTheRight(row, column) && neighbouringSquareContainsABomb(row, column + 1)) { //right
-                        bombsInTheVicinity++;
-                        updateBombDistanceValue(row, column, bombsInTheVicinity);
-                    }
-                    if (thereIsARowAbove(row) && neighbouringSquareContainsABomb(row - 1, column)) { //top
-                        bombsInTheVicinity++;
-                        updateBombDistanceValue(row, column, bombsInTheVicinity);
-                    }
-                    if (thereIsARowBelow(row) && neighbouringSquareContainsABomb(row + 1, column)) { //bottom
-                        bombsInTheVicinity++;
-                        updateBombDistanceValue(row, column, bombsInTheVicinity);
-                    }
-                    if (thereIsAColumnToTheRight(row, column) && thereIsARowAbove(row) && neighbouringSquareContainsABomb(row - 1, column + 1)) {//top right
-                        bombsInTheVicinity++;
-                        updateBombDistanceValue(row, column, bombsInTheVicinity);
-                    }
-                    if (thereIsAColumnToTheLeft(column)  && thereIsARowAbove(row) && neighbouringSquareContainsABomb(row - 1, column - 1)) { //top left
-                        bombsInTheVicinity++;
-                        updateBombDistanceValue(row, column, bombsInTheVicinity);
-                    }
-                    if (thereIsARowBelow(row) && thereIsAColumnToTheRight(row, column) && neighbouringSquareContainsABomb(row + 1, column + 1)) { //bottom right
-                        bombsInTheVicinity++;
-                        updateBombDistanceValue(row, column, bombsInTheVicinity);
-                    }
-                    if (thereIsARowBelow(row) && thereIsAColumnToTheLeft(column) && neighbouringSquareContainsABomb(row + 1, column - 1)) { //bottom left
-                        bombsInTheVicinity++;
-                        updateBombDistanceValue(row, column, bombsInTheVicinity);
-                    }
-                }
+                calculateBombsSurroundingASquare(row, column);
             }
-
         }
     }
 
-    private boolean thereIsAColumnToTheLeft(int j) {
-        return thereIsARowAbove(j);
+    private void calculateBombsSurroundingASquare(int row, int column) {
+        int bombsInTheVicinity = 0;
+        for (int i = 0; i < bombCheckerConditions(row,column).length; i++ ){
+            if (bombCheckerConditions(row,column)[i]) {
+                bombsInTheVicinity++;
+                updateBombDistanceValue(row, column, bombsInTheVicinity);
+            }
+        }
+    }
+
+    private boolean [] bombCheckerConditions(int row, int column) {
+        boolean[] bombChecks = new boolean[8];
+        if(board[row][column] != '*') {
+            bombChecks[0] = thereIsABombToTheLeft(row, column);
+            bombChecks[1] = thereIsABombToTheRight(row, column);
+            bombChecks[2] = thereIsABombAbove(row, column);
+            bombChecks[3] = thereIsABombBelow(row, column);
+            bombChecks[4] = thereIsABombToTheTopRight(row, column);
+            bombChecks[5] = thereIsABombToTheTopLeft(row, column);
+            bombChecks[6] = thereIsABombToTheBottomRight(row, column);
+            bombChecks[7] = thereIsABombToTheBottomLeft(row, column);
+        }
+        return bombChecks;
+    }
+
+    private boolean thereIsABombToTheLeft(int row, int column) {
+        return thereIsAColumnToTheLeft(column) && neighbouringSquareContainsABomb(row, column - 1);
+    }
+
+    private boolean thereIsABombToTheRight(int row, int column) {
+        return thereIsAColumnToTheRight(row, column) && neighbouringSquareContainsABomb(row, column + 1);
+    }
+
+    private boolean thereIsABombAbove(int row, int column) {
+        return thereIsARowAbove(row) && neighbouringSquareContainsABomb(row - 1, column);
+    }
+
+    private boolean thereIsABombBelow(int row, int column) {
+        return thereIsARowBelow(row) && neighbouringSquareContainsABomb(row + 1, column);
+    }
+
+    private boolean thereIsABombToTheTopRight(int row, int column) {
+        return thereIsAColumnToTheRight(row, column) && thereIsARowAbove(row) && neighbouringSquareContainsABomb(row - 1, column + 1);
+    }
+
+    private boolean thereIsABombToTheTopLeft(int row, int column) {
+        return thereIsAColumnToTheLeft(column)  && thereIsARowAbove(row) && neighbouringSquareContainsABomb(row - 1, column - 1);
+    }
+
+    private boolean thereIsABombToTheBottomRight(int row, int column) {
+        return thereIsARowBelow(row) && thereIsAColumnToTheRight(row, column) && neighbouringSquareContainsABomb(row + 1, column + 1);
+    }
+
+    private boolean thereIsABombToTheBottomLeft(int row, int column) {
+        return thereIsARowBelow(row) && thereIsAColumnToTheLeft(column) && neighbouringSquareContainsABomb(row + 1, column - 1);
+    }
+
+    private boolean thereIsAColumnToTheLeft(int columnIndex) {
+        return columnIndex - 1 >= 0;
     }
 
     private boolean thereIsAColumnToTheRight(int rowIndex, int columnIndex) {
         return board[rowIndex].length > columnIndex + 1;
     }
 
-    private boolean thereIsARowAbove(int row) {
-        return row - 1 >= 0;
+    private boolean thereIsARowAbove(int rowIndex) {
+        return rowIndex - 1 >= 0;
     }
 
-    private boolean thereIsARowBelow(int row) {
-        return row + 1 < board.length;
+    private boolean thereIsARowBelow(int rowIndex) {
+        return rowIndex + 1 < board.length;
     }
 
     private boolean neighbouringSquareContainsABomb(int rowIndex, int columnIndex) {
